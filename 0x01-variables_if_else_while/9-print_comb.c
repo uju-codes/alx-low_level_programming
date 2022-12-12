7c063b6f24be6825f14debc01ae22993003fa374
uju-codes <obianuju.obeta1@gmail.com>
2022-12-12 10:09:54 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,25 +2,21 @@
 #include <stdlib.h>
 
 /**
- * main - prints all the numbers of base 16 in lowercase
+ * main - prints all the combination of single digits
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int num = '0'; /*Initialize num to 0*/
-	char letter = 'a'; /*Initialize letter to a*/
+	int n;
 
-	while (num <= '9') /*Print 0-9*/
+	for (n = 48; n < 58; n++)
 	{
-		putchar(num);
-		num++;
-	}
-	while (letter <= 'f') /*Print a-f*/
-	{
-		putchar(letter);
-		letter++;
-	}
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	putchar('\n');
 	return (0);
 }
