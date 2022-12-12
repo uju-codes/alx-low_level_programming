d99e77119fe4ed59a50e0d81c7953744e033984d
uju-codes <obianuju.obeta1@gmail.com>
2022-12-12 10:40:40 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,18 @@
  */
 int main(void)
 {
-	int n;
+	int c = 0;
 
-	for (n = 48; n < 58; n++)
+	while (c < 10)
 	{
-		putchar(n);
-	}
-		if (n != 57)
+		putchar(48 + c);
+		if (c != 9)
 		{
-			putchar(',');
+			putchar(',')
+			putchar(' ');
 		}
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
