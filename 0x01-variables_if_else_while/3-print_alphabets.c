f5e19ce505616012fd5255dfa64c6d78c476ba7a
uju-codes <obianuju.obeta1@gmail.com>
2022-12-12 03:04:16 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabets in lower and upper case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
