7ff31ba3148617dbda25f0959d031f8a38d0d4bc
uju-codes <obianuju.obeta1@gmail.com>
2022-12-13 14:38:23 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function that prints alphabet in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
