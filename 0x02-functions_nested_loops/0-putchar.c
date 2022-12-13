07e2555232ce285b0e60486dec0bf3b4c7ee785c
uju-codes <obianuju.obeta1@gmail.com>
2022-12-13 14:17:41 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main -  a program that prints _putchar and a new line
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	_putchar(',');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
