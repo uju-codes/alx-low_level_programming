b0f2a0425f6cb215af761512d6248f1afa99c243
uju-codes <obianuju.obeta1@gmail.com>
2022-12-13 16:37:38 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character check
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+
+		return (1);
+	else
+		return (1);
+}
