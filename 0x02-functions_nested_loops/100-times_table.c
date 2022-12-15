652621aba5013d34f636166fbd3acde775e3da66
uju-codes <obianuju.obeta1@gmail.com>
2022-12-15 03:57:16 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,22 +26,27 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar('0' + product);
-			}
-			else if (product > 9 && product < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + (product / 10));
-				_putchar('0' + (product % 10));
-			}
-			else if (product >= 100)
-			{
-				_putchar('0' + (product / 100));
-				_putchar('0' + ((product / 10) % 10));
-				_putchar('0' + (product % 10));
+				if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
