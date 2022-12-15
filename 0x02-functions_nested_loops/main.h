52211554c0649ede57039cb30a22954962c21f42
uju-codes <obianuju.obeta1@gmail.com>
2022-12-15 04:25:33 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,8 +12,9 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char);
+int _putZZchar(char);
 int print_times_table(int n);
 int _putchar(char c);
-void print_times_table(int n)
+void print_times_table(int n);
+
 #endif
